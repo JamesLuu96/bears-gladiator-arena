496a7d19c22dc942836d476f6cd3db2b87904792
JamesLuu96 <32526792+JamesLuu96@users.noreply.github.com>
2022-07-19 14:47:25 -0500

    pwa

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,35 @@
+const GHPATH = '/bears-gladiator-arena'
+const APP_PREFIX = 'bga_'
+const VERSION = 'version_00'
+const URLS = [
+    `${GHPATH}/`,
+    `${GHPATH}/script.js`,
+    `${GHPATH}/bear-head.jpg`,
+    `${GHPATH}/styles.css`,
+    `${GHPATH}/assets/pics/baby-bear-1`,
+    `${GHPATH}/assets/pics/baby-bear-2`,
+    `${GHPATH}/assets/pics/baby-bear-3`,
+    `${GHPATH}/assets/pics/baby-bear-4`,
+    `${GHPATH}/assets/pics/baby-bear-5`,
+    `${GHPATH}/assets/pics/baby-bear-6`,
+    `${GHPATH}/assets/pics/baby-bear-7`,
+    `${GHPATH}/assets/pics/mama-bear-1`,
+    `${GHPATH}/assets/pics/mama-bear-2`,
+    `${GHPATH}/assets/pics/mama-bear-3`,
+    `${GHPATH}/assets/pics/mama-bear-4`,
+    `${GHPATH}/assets/pics/mama-bear-5`,
+    `${GHPATH}/assets/pics/mama-bear-6`,
+    `${GHPATH}/assets/pics/mama-bear-7`,
+    `${GHPATH}/assets/pics/papa-bear-1`,
+    `${GHPATH}/assets/pics/papa-bear-2`,
+    `${GHPATH}/assets/pics/papa-bear-3`,
+    `${GHPATH}/assets/pics/papa-bear-4`,
+    `${GHPATH}/assets/pics/papa-bear-5`,
+    `${GHPATH}/assets/pics/papa-bear-6`,
+    `${GHPATH}/assets/pics/papa-bear-7`,
+    `${GHPATH}/icons`
+]
+
 self.addEventListener("install", e=>{
     console.log("installed")
     e.waitUntil(
